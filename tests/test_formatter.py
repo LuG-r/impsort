74b7ff833b1b98d21974ef14557a80aed64cb14b
Lucas G <lucasg@lucass-air.home>
2024-06-06 22:06:53 +0100

    test written for basic conditional imports

--- tests/test_formatter.py
+++ tests/test_formatter.py
@@ -24,9 +24,19 @@ def test_comments(tmp_path):
 
 
 def test_docstring(tmp_path):
-    test_content = "\"\"\"This is an example of a file with a module docstring at the top\nrather than imports.\n\"\"\"\nimport numpy as np\nimport matplotlib.pyplot as plt\nfrom os import path\nimport requests\nfrom delocate.fuse import wheels\nimport argparse\n\nif __name__ == '__main__':\n    print('bad juju')"
-    target_content = "\"\"\"This is an example of a file with a module docstring at the top\nrather than imports.\n\"\"\"\nimport argparse\nimport matplotlib.pyplot as plt\nimport numpy as np\nimport requests\nfrom delocate.fuse import wheels\nfrom os import path\n\nif __name__ == '__main__':\n    print('bad juju')"
+    test_content = '"""This is an example of a file with a module docstring at the top\nrather than imports.\n"""\nimport numpy as np\nimport matplotlib.pyplot as plt\nfrom os import path\nimport requests\nfrom delocate.fuse import wheels\nimport argparse\n\nif __name__ == \'__main__\':\n    print(\'bad juju\')'
+    target_content = '"""This is an example of a file with a module docstring at the top\nrather than imports.\n"""\nimport argparse\nimport matplotlib.pyplot as plt\nimport numpy as np\nimport requests\nfrom delocate.fuse import wheels\nfrom os import path\n\nif __name__ == \'__main__\':\n    print(\'bad juju\')'
     p = tmp_path / "simple.py"
+    p.write_text(test_content)
+    assert p.read_text() == test_content
+    FORMATTER.format_file(str(p))
+    assert p.read_text() == target_content
+
+
+def test_conditional(tmp_path):
+    test_content = "import argparse\n\nif __name__ == '__main__':\n    test_conditional = True\n    if test_conditional:\n        import os\n        print('bad juju')"
+    target_content = "import argparse\n\nif __name__ == '__main__':\n    test_conditional = True\n    if test_conditional:\n        import os\n        print('bad juju')"
+    p = tmp_path / "conditional.py"
     p.write_text(test_content)
     assert p.read_text() == test_content
     FORMATTER.format_file(str(p))
