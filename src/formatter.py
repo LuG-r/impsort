e92f68c809e9565ff1ac9aea16ece7845980b2d0
Lucas G <lucasg@lucass-air.home>
2024-06-06 21:39:53 +0100

    app moved to /src directory, functionality moved to Formatter class

---
+++ src/formatter.py
@@ -0,0 +1,55 @@
+class Formatter:
+    def __init__(
+        self, is_preview: bool = False, is_check_semantic: bool = False
+    ) -> None:
+        self.is_preview = is_preview
+        self.is_check_semantic = is_check_semantic
+
+    def format_file(self, path_to_file: str):
+        if path_to_file[-3:] != ".py":
+            raise ValueError("The file provided does not appear to be a Python file")
+        self.organise_packages(self.get_package_info(path_to_file))
+
+    def get_package_info(self, file_path: str) -> object:
+        packages = {"import": [], "from": [], "import_lines": [], "lines": []}
+        line_number = 1
+        with open(file_path, "r") as f:
+            for line in f:
+                line_contents = line.split(" ")
+                if line_contents[0] == "import":
+                    packages["import"].append(
+                        {"package_name": line_contents[1], "line": line}
+                    )
+                    packages["import_lines"].append(line_number)
+                elif line_contents[0] == "from":
+                    packages["from"].append(
+                        {"package_name": line_contents[1], "line": line}
+                    )
+                    packages["import_lines"].append(line_number)
+                else:
+                    packages["lines"].append(line)
+                line_number += 1
+        packages["import"].sort(key=lambda x: x["package_name"])
+        packages["from"].sort(key=lambda x: x["package_name"])
+        packages["import"].extend(packages["from"])
+
+        return {
+            "file_path": file_path,
+            "packages": packages["import"],
+            "line_numbers": packages["import_lines"],
+            "lines": packages["lines"],
+        }
+
+    def organise_packages(self, packages: object) -> None:
+        base_content_position = 0
+        current_line_number = 1
+        with open(packages["file_path"], "w") as f:
+            for index, target_line_number in enumerate(packages["line_numbers"]):
+                while current_line_number != target_line_number:
+                    f.write(packages["lines"][base_content_position])
+                    base_content_position += 1
+                    current_line_number += 1
+                f.write(packages["packages"][index]["line"])
+                current_line_number += 1
+            if base_content_position < len(packages["lines"]):
+                f.writelines(packages["lines"][base_content_position:])
